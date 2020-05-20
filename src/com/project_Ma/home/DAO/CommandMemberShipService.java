6632c990281378a00855896d65430a8994a3804a
jungjinbeom <insunok0715@naver.com>
2020-05-20 11:34:26 +0900

    asd

--- src/com/project_Ma/home/DAO/CommandMemberShipService.java
+++ src/com/project_Ma/home/DAO/CommandMemberShipService.java
@@ -1,9 +1,11 @@
 package com.project_Ma.home.DAO;
 
+import com.project_Ma.home.VO.MemberVO;
+
 public interface CommandMemberShipService {
 	//회원가입(유저)
-	public int userInsert(int user_no);
+	public int userInsert(MemberVO vo);
 	//회원가입(기업)
-	public int coporateInsert(int user_no);
+	public int coporateInsert(MemberVO vo);
 	
 }
